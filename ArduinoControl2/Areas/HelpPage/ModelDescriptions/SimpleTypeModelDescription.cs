576d5ae4a1c0bdeaad16cd248991197c86842ee3
Sridhar Sathya <sridhar.sathya@live.com>
2015-06-17 20:15:38 -0400

    Solution created and packages updated.

---
+++ ArduinoControl2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ArduinoControl2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ArduinoControl2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ArduinoControl2.Startup))]
+
+namespace ArduinoControl2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
